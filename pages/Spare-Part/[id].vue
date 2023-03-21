5877e1b162d7476039c76cbac81693a343b82213
kukwa clovis <kukwaclovisngong3@gmail.com>
2023-03-21 10:10:25 +0100

    march 21 stable commit

--- pages/Spare-Part/[id].vue
+++ pages/Spare-Part/[id].vue
@@ -1,19 +1,45 @@
 <template>
   <div class="product-main">
-    <div class="product-page-wrapper">
+    <div class="product-main-wrapper">
       <div class="product-image">
-        <img src="assets/engine/engine.jpg" alt="" />
+        <img :src="`/_nuxt/assets/car-engines/${productRoute}.jpeg`" alt="" />
       </div>
 
       <div class="product-details">
         <div class="product-details-wrapper">
-          <h2>product name</h2>
+          <h2>{{ productRoute }}</h2>
           <h4>product category</h4>
           <p>
             product description Lorem ipsum, dolor sit amet consectetur
             adipisicing elit. Inventore, pariatur!
           </p>
           <button>buy now</button>
+        </div>
+      </div>
+    </div>
+    <div class="other-products">
+      <h1>find other products taht interest you</h1>
+      <div class="products-div">
+        <div
+          class="product"
+          v-for="product in products"
+          :key="product.id"
+          @click="($event) => navigateTo(`/spare-part/${product.imgName}`)"
+        >
+          <div class="product-image">
+            <img
+              :src="
+                '/_nuxt/assets/car-engines/' +
+                product.imgName +
+                '.' +
+                product.imgExt
+              "
+              alt=""
+            />
+          </div>
+          <div class="route-div">
+            <button>view product</button>
+          </div>
         </div>
       </div>
     </div>
@@ -24,12 +50,142 @@
 const route = useRoute();
 
 const productRoute = route.params.id;
+
+const products = ref([]);
+
+for (let i = 0; i < 15; i++) {
+  let newImage = {
+    id: i,
+    name: "engine shaft",
+    category: "engine",
+    description:
+      "this is an engine with a difference good and affordable with quality and insurrance.",
+    imgurl: "",
+    imgName: "car-generator",
+    imgExt: "jpeg",
+  };
+  products.value.push(newImage);
+}
 </script>
 
 <style lang="scss" scoped>
 .product-main {
   width: 100%;
   height: fit-content;
-  padding-top: 13vh;
+  padding-top: 15vh;
+
+  .product-main-wrapper {
+    width: 100%;
+    height: fit-content;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+
+    .product-image {
+      width: 60%;
+      height: fit-content;
+
+      img {
+        width: 100%;
+        height: fit-content;
+        object-fit: contain;
+        cursor: pointer;
+      }
+    }
+
+    .product-details {
+      width: 35%;
+      height: fit-content;
+
+      .product-details-wrapper {
+        width: 100%;
+        height: fit-content;
+        padding: 20px 10px;
+
+        h2,
+        h4,
+        p {
+          text-align: left;
+        }
+
+        h2 {
+          text-transform: capitalize;
+        }
+
+        h4 {
+          text-transform: capitalize;
+        }
+
+        button {
+          width: max-content;
+          height: max-content;
+          padding: 8px 20px;
+          border-radius: 2px;
+          background: rgb(232, 152, 2);
+          color: white;
+          border: none;
+          display: block;
+          margin: 0;
+          margin-left: 10px;
+        }
+      }
+    }
+  }
+  .other-products {
+    width: 100%;
+    height: fit-content;
+    padding: 20px 10px;
+
+    h1 {
+      text-transform: capitalize;
+      font: 700 30px "Montserrat", "Poppins", sans-serif;
+      padding: 20px 10px;
+    }
+
+    .products-div {
+      width: 100%;
+      height: fit-content;
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      flex-wrap: wrap;
+      gap: 20px;
+
+      .product {
+        width: 200px;
+        height: 200px;
+        overflow: hidden;
+        box-shadow: inset 0 0 15px 8px rgb(230, 230, 230);
+
+        .product-image {
+          width: 100%;
+          height: 140px;
+
+          img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            cursor: pointer;
+          }
+        }
+        .route-div {
+          width: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+
+          button {
+            width: 80%;
+            height: 40px;
+            background: orange;
+            color: white;
+            border: none;
+            border-radius: 30px;
+            margin: 0 auto;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
---
+++ pages/Spare-Part/create-product.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="create-product-main">
+    <div class="create-product-wrapper">
+      <form @submit="createProduct">
+        <h1>create a custom product.</h1>
+
+        <div class="form-data">
+          <input
+            type="text"
+            name="product-name"
+            v-model="productData.name"
+            placeholder="Enter product name"
+            required
+          /><input
+            type="text"
+            name="product-category"
+            v-model="productData.category"
+            placeholder="Enter product category"
+            required
+          /><input
+            type="text"
+            name="product-description"
+            v-model="productData.description"
+            placeholder="Product Description"
+          /><input
+            type="text"
+            name="image-name"
+            v-model="productData.imgName"
+            placeholder="image name"
+            required
+          /><input type="text" name="image-uel" placeholder="imaeg url" />
+          <input
+            type="text"
+            name="image-extension"
+            v-model="productData.imgExt"
+            placeholder="image extension"
+            required
+          />
+        </div>
+        <button type="submit">create new product</button>
+        <button type="reset">clear input fields</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const productData = reactive({
+  id: "",
+  name: "",
+  category: "",
+  imgExt: "",
+  imgurl: "",
+  imgName: "",
+  description: "",
+});
+const useProductSate = products();
+
+const createProduct = (e) => {
+  e.preventDefault();
+  productData.id =
+    productData.imgName.toString("hex") +
+    Math.floor(Math.random() * 434) * 123 +
+    Math.ceil(Math.random() * 112);
+
+  useProductSate.value.push(productData);
+};
+
+console.log(useProductSate.value);
+</script>
+
+<style lang="scss" scoped>
+.create-product-main {
+  width: 100%;
+  height: fit-content;
+  padding-top: 14vh;
+
+  .create-product-wrapper {
+    width: 100%;
+    height: fit-content;
+
+    form {
+      width: 90%;
+      margin: 20px auto;
+
+      input {
+        width: 70%;
+        height: 40px;
+        margin: 15px auto;
+        border: none;
+        background: rgb(205, 205, 205);
+        display: block;
+      }
+
+      button {
+        width: max-content;
+        height: max-content;
+        padding: 20px;
+        border: none;
+        background: orange;
+        color: white;
+        margin: 10px auto;
+        display: block;
+      }
+    }
+  }
+}
+</style>
