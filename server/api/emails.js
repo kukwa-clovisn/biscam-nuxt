6ab63add618f212f0e7873de65e0448ca1853b26
kukwa clovis <kukwaclovisngong3@gmail.com>
2023-03-30 12:07:21 +0100

    march 29 working commit

--- server/api/emails.js
+++ server/api/emails.js
@@ -2,7 +2,6 @@ import sendVerificationGmail from "./verifyGmail";
 
 export default defineEventHandler(async (event) => {
   try {
-    console.log(event);
     return await sendVerificationGmail("codingherald@gmail.com");
   } catch (error) {
     return sendError(event, createError(error));
