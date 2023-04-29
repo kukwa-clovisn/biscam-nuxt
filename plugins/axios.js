cb5f25caf9be801e0dfc3ee14dad817c4e4d2051
kukwa clovis <kukwaclovisngong3@gmail.com>
2023-04-29 09:18:29 +0100

    ready for prod:

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  axios.defaults.baseURL = "http://localhost:3000";
+  // axios.defaults.baseURL = "http://localhost:3000";
   // axios.defaults.baseURL = "https://biscaminvestmentsarl.com";
+  axios.defaults.baseURL = "https://biscaminvestmentsarl.netlify.app";
 });
